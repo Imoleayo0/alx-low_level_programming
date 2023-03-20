36a7050a4d18d1712890689fb9b2b2449a5dcf3a
Imoleayo0 <abeayomide27@gmail.com>
2023-03-20 10:56:03 -0700

    error for last digit done

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - prints all single digit numbers of base
- * 10 starting from 0, followed by a new line
- * Return: Always 0 (Success)
+ *main - prints all single digit numbers of base
+ *10 starting from 0, followed by a new line
+ *Return: Always 0
  */
 
 int main(void)
 
 {
-	
 	int x;
 
 	for (x = 0; x < 10; x++)
